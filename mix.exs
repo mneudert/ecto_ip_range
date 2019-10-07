4e05431a1df58fd56b7eaee10971d1f22409f3e9
Marc Neudert <marc.neudert@gmail.com>
2019-10-07 18:37:32 +0200

    Install required dependencies

--- mix.exs
+++ mix.exs
@@ -29,8 +29,10 @@ defmodule EctoIPRange.MixProject do
     [
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
+      {:ecto, "~> 3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.11", only: :test, runtime: false}
+      {:excoveralls, "~> 0.11", only: :test, runtime: false},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
