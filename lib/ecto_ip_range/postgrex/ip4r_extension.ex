6b2f95989c56eb20c2a7723e20be7c9b271a6862
Marc Neudert <marc.neudert@gmail.com>
2019-10-22 20:07:25 +0200

    Allow insert/select for single IP networks

--- lib/ecto_ip_range/postgrex/ip4r_extension.ex
+++ lib/ecto_ip_range/postgrex/ip4r_extension.ex
@@ -22,6 +22,17 @@ defmodule EctoIPRange.Postgrex.IP4RExtension do
 
   def decode(_) do
     quote location: :keep do
+      <<8::int32, first_a, first_b, first_c, first_d, first_a, first_b, first_c, first_d>> ->
+        first_ip_address = {first_a, first_b, first_c, first_d}
+
+        with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip_address) do
+          %IP4R{
+            range: first_ip <> "/32",
+            first_ip: first_ip_address,
+            last_ip: first_ip_address
+          }
+        end
+
       <<8::int32, first_a, first_b, first_c, first_d, last_a, last_b, last_c, last_d>> ->
         first_ip_address = {first_a, first_b, first_c, first_d}
         last_ip_address = {last_a, last_b, last_c, last_d}
--- test/ecto_ip_range/postgrex/ip4r_extension_test.exs
+++ test/ecto_ip_range/postgrex/ip4r_extension_test.exs
@@ -4,6 +4,14 @@ defmodule EctoIPRange.Postgrex.IP4RExtensionTest do
   alias EctoIPRange.IP4R
   alias EctoIPRange.TestSchemaIP4R
 
+  test "single ip insert/select" do
+    {:ok, network} = IP4R.cast("1.2.3.4")
+    record = %TestSchemaIP4R{network: network}
+
+    assert {:ok, value} = TestRepo.insert(record)
+    assert [^value] = TestRepo.all(TestSchemaIP4R)
+  end
+
   test "custom ip range insert/select" do
     {:ok, network} = IP4R.cast("1.2.3.4-2.3.4.5")
     record = %TestSchemaIP4R{network: network}
