c397fe1ca985ad6e1b5091ff9c50287648b90afb
Marc Neudert <marc.neudert@gmail.com>
2019-10-31 19:26:56 +0100

    Add tests for IP utility functions

--- lib/ecto_ip_range/util/inet.ex
+++ lib/ecto_ip_range/util/inet.ex
@@ -3,6 +3,20 @@ defmodule EctoIPRange.Util.Inet do
 
   @doc """
   Convert an IP tuple to a binary address.
+
+  ## Examples
+
+      iex> ntoa({1, 2, 3, 4})
+      "1.2.3.4"
+
+      iex> ntoa({1, 2, 3, 4, 5, 6, 7, 8})
+      "1:2:3:4:5:6:7:8"
+
+      iex> ntoa("1.2.3.4")
+      nil
+
+      iex> ntoa("a.b.c.d")
+      nil
   """
   @spec ntoa(:inet.ip_address()) :: binary | nil
   def ntoa(ip_address) do
@@ -14,6 +28,17 @@ defmodule EctoIPRange.Util.Inet do
 
   @doc """
   Parse a binary IPv4 address.
+
+  ## Examples
+
+      iex> parse_ipv4_binary("1.2.3.4")
+      {:ok, {1, 2, 3, 4}}
+
+      iex> parse_ipv4_binary("::1")
+      {:error, :einval}
+
+      iex> parse_ipv4_binary("a.b.c.d")
+      {:error, :einval}
   """
   @spec parse_ipv4_binary(binary) :: {:ok, :inet.ip4_address()} | {:error, :einval}
   def parse_ipv4_binary(address) do
---
+++ test/ecto_ip_range/util/inet_test.exs
@@ -0,0 +1,5 @@
+defmodule EctoIPRange.Util.InetTest do
+  use ExUnit.Case, async: true
+
+  doctest EctoIPRange.Util.Inet, import: true
+end
