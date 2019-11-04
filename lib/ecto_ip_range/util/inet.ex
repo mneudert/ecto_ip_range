e8780b8de3c76e7468d6de69067c910ba89736b5
Marc Neudert <marc.neudert@gmail.com>
2019-11-04 18:59:37 +0100

    Extend IP utility to handle IPv6

--- lib/ecto_ip_range/util/inet.ex
+++ lib/ecto_ip_range/util/inet.ex
@@ -34,10 +34,13 @@ defmodule EctoIPRange.Util.Inet do
       iex> parse_ipv4_binary("1.2.3.4")
       {:ok, {1, 2, 3, 4}}
 
-      iex> parse_ipv4_binary("::1")
+      iex> parse_ipv4_binary("1:2:3:4:5:6:7:8")
       {:error, :einval}
 
       iex> parse_ipv4_binary("a.b.c.d")
+      {:error, :einval}
+
+      iex> parse_ipv4_binary("s:t:u:v:w:x:y:z")
       {:error, :einval}
   """
   @spec parse_ipv4_binary(binary) :: {:ok, :inet.ip4_address()} | {:error, :einval}
@@ -45,5 +48,29 @@ defmodule EctoIPRange.Util.Inet do
     address
     |> String.to_charlist()
     |> :inet.parse_ipv4_address()
+  end
+
+  @doc """
+  Parse a binary IPv6 address.
+
+  ## Examples
+
+      iex> parse_ipv6_binary("1.2.3.4")
+      {:ok, {0, 0, 0, 0, 0, 65535, 258, 772}}
+
+      iex> parse_ipv6_binary("1:2:3:4:5:6:7:8")
+      {:ok, {1, 2, 3, 4, 5, 6, 7, 8}}
+
+      iex> parse_ipv6_binary("a.b.c.d")
+      {:error, :einval}
+
+      iex> parse_ipv6_binary("s:t:u:v:w:x:y:z")
+      {:error, :einval}
+  """
+  @spec parse_ipv6_binary(binary) :: {:ok, :inet.ip6_address()} | {:error, :einval}
+  def parse_ipv6_binary(address) do
+    address
+    |> String.to_charlist()
+    |> :inet.parse_ipv6_address()
   end
 end
