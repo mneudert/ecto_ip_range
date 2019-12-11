437ce6e866ddad5c5981f39207a66af399f52127
Marc Neudert <marc.neudert@gmail.com>
2019-12-11 19:58:17 +0100

    Silence "clause grouping" compiler warning

--- lib/ecto_ip_range/postgrex/ip4r_extension.ex
+++ lib/ecto_ip_range/postgrex/ip4r_extension.ex
@@ -22,31 +22,34 @@ defmodule EctoIPRange.Postgrex.IP4RExtension do
 
   def decode(_) do
     quote location: :keep do
-      <<8::int32, first_a, first_b, first_c, first_d, first_a, first_b, first_c, first_d>> ->
-        first_ip4_address = {first_a, first_b, first_c, first_d}
-
-        with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip4_address) do
-          %IP4R{
-            range: first_ip <> "/32",
-            first_ip: first_ip4_address,
-            last_ip: first_ip4_address
-          }
-        end
-
       <<8::int32, first_a, first_b, first_c, first_d, last_a, last_b, last_c, last_d>> ->
-        first_ip4_address = {first_a, first_b, first_c, first_d}
-        last_ip4_address = {last_a, last_b, last_c, last_d}
+        unquote(__MODULE__).decode(
+          {first_a, first_b, first_c, first_d},
+          {last_a, last_b, last_c, last_d}
+        )
+    end
+  end
 
-        with(
-          first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip4_address),
-          last_ip when is_binary(last_ip) <- Inet.ntoa(last_ip4_address)
-        ) do
-          %IP4R{
-            range: first_ip <> "-" <> last_ip,
-            first_ip: first_ip4_address,
-            last_ip: last_ip4_address
-          }
-        end
+  @doc false
+  @spec decode(:inet.ip4_address(), :inet.ip4_address()) :: IP4R.t()
+  def decode(first_ip4_address, first_ip4_address) do
+    with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip4_address) do
+      %IP4R{
+        range: first_ip <> "/32",
+        first_ip: first_ip4_address,
+        last_ip: first_ip4_address
+      }
+    end
+  end
+
+  def decode(first_ip4_address, last_ip4_address) do
+    with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip4_address),
+         last_ip when is_binary(last_ip) <- Inet.ntoa(last_ip4_address) do
+      %IP4R{
+        range: first_ip <> "-" <> last_ip,
+        first_ip: first_ip4_address,
+        last_ip: last_ip4_address
+      }
     end
   end
 end
--- lib/ecto_ip_range/postgrex/ip6r_extension.ex
+++ lib/ecto_ip_range/postgrex/ip6r_extension.ex
@@ -26,39 +26,36 @@ defmodule EctoIPRange.Postgrex.IP6RExtension do
   def decode(_) do
     quote location: :keep do
       <<32::int32, first_a::int16, first_b::int16, first_c::int16, first_d::int16, first_e::int16,
-        first_f::int16, first_g::int16, first_h::int16, first_a::int16, first_b::int16,
-        first_c::int16, first_d::int16, first_e::int16, first_f::int16, first_g::int16,
-        first_h::int16>> ->
-        first_ip6_address =
-          {first_a, first_b, first_c, first_d, first_e, first_f, first_g, first_h}
-
-        with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip6_address) do
-          %IP6R{
-            range: first_ip <> "/128",
-            first_ip: first_ip6_address,
-            last_ip: first_ip6_address
-          }
-        end
-
-      <<32::int32, first_a::int16, first_b::int16, first_c::int16, first_d::int16, first_e::int16,
         first_f::int16, first_g::int16, first_h::int16, last_a::int16, last_b::int16,
         last_c::int16, last_d::int16, last_e::int16, last_f::int16, last_g::int16,
         last_h::int16>> ->
-        first_ip6_address =
-          {first_a, first_b, first_c, first_d, first_e, first_f, first_g, first_h}
+        unquote(__MODULE__).decode(
+          {first_a, first_b, first_c, first_d, first_e, first_f, first_g, first_h},
+          {last_a, last_b, last_c, last_d, last_e, last_f, last_g, last_h}
+        )
+    end
+  end
 
-        last_ip6_address = {last_a, last_b, last_c, last_d, last_e, last_f, last_g, last_h}
+  @doc false
+  @spec decode(:inet.ip6_address(), :inet.ip6_address()) :: IP6R.t()
+  def decode(first_ip6_address, first_ip6_address) do
+    with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip6_address) do
+      %IP6R{
+        range: first_ip <> "/128",
+        first_ip: first_ip6_address,
+        last_ip: first_ip6_address
+      }
+    end
+  end
 
-        with(
-          first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip6_address),
-          last_ip when is_binary(last_ip) <- Inet.ntoa(last_ip6_address)
-        ) do
-          %IP6R{
-            range: first_ip <> "-" <> last_ip,
-            first_ip: first_ip6_address,
-            last_ip: last_ip6_address
-          }
-        end
+  def decode(first_ip6_address, last_ip6_address) do
+    with first_ip when is_binary(first_ip) <- Inet.ntoa(first_ip6_address),
+         last_ip when is_binary(last_ip) <- Inet.ntoa(last_ip6_address) do
+      %IP6R{
+        range: first_ip <> "-" <> last_ip,
+        first_ip: first_ip6_address,
+        last_ip: last_ip6_address
+      }
     end
   end
 end
