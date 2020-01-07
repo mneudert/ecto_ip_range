2934e075db0b655cee7ed74c926a85a945050d31
Marc Neudert <marc.neudert@gmail.com>
2020-01-07 17:51:55 +0100

    Fix integer type used for IP6R encoding/decoding

--- lib/ecto_ip_range/postgrex/ip6r_extension.ex
+++ lib/ecto_ip_range/postgrex/ip6r_extension.ex
@@ -16,19 +16,27 @@ defmodule EctoIPRange.Postgrex.IP6RExtension do
         first_ip: {first_a, first_b, first_c, first_d, first_e, first_f, first_g, first_h},
         last_ip: {last_a, last_b, last_c, last_d, last_e, last_f, last_g, last_h}
       } ->
-        <<32::int32, first_a::int16, first_b::int16, first_c::int16, first_d::int16,
-          first_e::int16, first_f::int16, first_g::int16, first_h::int16, last_a::int16,
-          last_b::int16, last_c::int16, last_d::int16, last_e::int16, last_f::int16,
-          last_g::int16, last_h::int16>>
+        <<32::int32, first_a::size(16)-integer-unsigned, first_b::size(16)-integer-unsigned,
+          first_c::size(16)-integer-unsigned, first_d::size(16)-integer-unsigned,
+          first_e::size(16)-integer-unsigned, first_f::size(16)-integer-unsigned,
+          first_g::size(16)-integer-unsigned, first_h::size(16)-integer-unsigned,
+          last_a::size(16)-integer-unsigned, last_b::size(16)-integer-unsigned,
+          last_c::size(16)-integer-unsigned, last_d::size(16)-integer-unsigned,
+          last_e::size(16)-integer-unsigned, last_f::size(16)-integer-unsigned,
+          last_g::size(16)-integer-unsigned, last_h::size(16)-integer-unsigned>>
     end
   end
 
   def decode(_) do
     quote location: :keep do
-      <<32::int32, first_a::int16, first_b::int16, first_c::int16, first_d::int16, first_e::int16,
-        first_f::int16, first_g::int16, first_h::int16, last_a::int16, last_b::int16,
-        last_c::int16, last_d::int16, last_e::int16, last_f::int16, last_g::int16,
-        last_h::int16>> ->
+      <<32::int32, first_a::size(16)-integer-unsigned, first_b::size(16)-integer-unsigned,
+        first_c::size(16)-integer-unsigned, first_d::size(16)-integer-unsigned,
+        first_e::size(16)-integer-unsigned, first_f::size(16)-integer-unsigned,
+        first_g::size(16)-integer-unsigned, first_h::size(16)-integer-unsigned,
+        last_a::size(16)-integer-unsigned, last_b::size(16)-integer-unsigned,
+        last_c::size(16)-integer-unsigned, last_d::size(16)-integer-unsigned-unsigned,
+        last_e::size(16)-integer-unsigned, last_f::size(16)-integer-unsigned,
+        last_g::size(16)-integer-unsigned, last_h::size(16)-integer-unsigned>> ->
         unquote(__MODULE__).decode(
           {first_a, first_b, first_c, first_d, first_e, first_f, first_g, first_h},
           {last_a, last_b, last_c, last_d, last_e, last_f, last_g, last_h}
