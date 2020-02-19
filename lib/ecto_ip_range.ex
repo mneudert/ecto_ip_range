eb87a8a1a294af531820b68a06e4cca60278ff15
Marc Neudert <marc.neudert@gmail.com>
2020-02-19 18:25:24 +0100

    Document ecto/postgrex setup

--- lib/ecto_ip_range.ex
+++ lib/ecto_ip_range.ex
@@ -1,5 +1,17 @@
 defmodule EctoIPRange do
   @moduledoc """
-  Ecto IP Range.
+  ## Setup
+
+  Define a module with the required `:postgrex` extensions added:
+
+      Postgrex.Types.define(
+        MyApp.PostgrexTypes,
+        EctoIPRange.Postgrex.extensions() ++ Ecto.Adapters.Postgres.extensions()
+      )
+
+  Add this module to your `:ecto` repo configuration:
+
+      config :my_app, MyRepo,
+        types: MyApp.PostgrexTypes
   """
 end
