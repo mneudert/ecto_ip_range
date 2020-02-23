9ec316068b9fa0d32e096d925fb03e9766fb50c9
Marc Neudert <marc.neudert@gmail.com>
2020-02-23 12:18:45 +0100

    Document usage in schemas

--- lib/ecto_ip_range.ex
+++ lib/ecto_ip_range.ex
@@ -16,5 +16,26 @@ defmodule EctoIPRange do
 
       config :my_app, MyRepo,
         types: MyApp.PostgrexTypes
+
+  ## Usage
+
+  Define the appropriate field type in your schema:
+
+      defmodule MySchema do
+        use Ecto.Schema
+
+        schema "test_schema_ip4" do
+          :my_ip_range_field, EctoIPRange.IP4
+        end
+      end
+
+  The following fields are currently supported:
+
+  - Single Addresses
+    - `EctoIPRange.IP4`
+    - `EctoIPRange.IP6`
+  - Address Ranges
+    - `EctoIPRange.IP4R`
+    - `EctoIPRange.IP6R`
   """
 end
