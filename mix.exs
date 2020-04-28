1076c63b6900a6c212dbc6d4e454b9e028e0d91e
Marc Neudert <marc.neudert@gmail.com>
2020-04-28 17:24:10 +0200

    Loosen :dialyxir requirement

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule EctoIPRange.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ecto_sql, "~> 3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12", only: :test, runtime: false},
