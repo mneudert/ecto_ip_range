1b79e9bc841ac46352011ab4ee53ccef38e773f2
Marc Neudert <marc.neudert@gmail.com>
2021-02-25 18:09:11 +0100

    Use GitHub for automated testing

--- mix.exs
+++ mix.exs
@@ -18,8 +18,7 @@ defmodule EctoIPRange.MixProject do
       package: package(),
       preferred_cli_env: [
         coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.travis": :test
+        "coveralls.detail": :test
       ],
       test_coverage: [tool: ExCoveralls]
     ]
