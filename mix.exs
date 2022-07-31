ff987696d63f96bd92917ba9d7d384f387599874
Marc Neudert <marc.neudert@gmail.com>
2022-07-31 10:33:40 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -38,8 +38,8 @@ defmodule EctoIPRange.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ecto_sql, "~> 3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
